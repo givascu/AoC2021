0dac2e785c6a11715391de29dd447fffa7b37250
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2021-12-11 22:51:40 +0200

    Some code improvements

--- src/day09.rs
+++ src/day09.rs
@@ -5,39 +5,25 @@ use std::{
 
 type Point = (usize, usize);
 
-fn get_neighbor_points(map: &[String], pos: Point) -> Vec<Point> {
-    let mut neighbors = Vec::new();
-
-    if pos.0 > 0 {
-        neighbors.push((pos.0 - 1, pos.1));
-    }
-    if pos.0 < map.len() - 1 {
-        neighbors.push((pos.0 + 1, pos.1));
-    }
-    if pos.1 > 0 {
-        neighbors.push((pos.0, pos.1 - 1));
-    }
-    if pos.1 < map[0].len() - 1 {
-        neighbors.push((pos.0, pos.1 + 1));
-    }
-
-    neighbors
+fn get_neighbors(p: Point, y_max: usize, x_max: usize) -> Vec<Point> {
+    [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        .iter()
+        .map(|&(dy, dx)| (p.0 as i32 + dy, p.1 as i32 + dx))
+        .filter(|&(y, x)| (0..y_max).contains(&(y as usize)) && (0..x_max).contains(&(x as usize)))
+        .map(|(y, x)| (y as usize, x as usize))
+        .collect()
 }
 
-fn get_low_points(map: &[String]) -> Vec<Point> {
+fn get_lows(map: &[String]) -> Vec<Point> {
     let mut low_points = Vec::new();
-
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            if get_neighbor_points(map, (y, x))
-                .iter()
-                .all(|p| map[p.0].chars().nth(p.1).unwrap() > map[y].chars().nth(x).unwrap())
-            {
-                low_points.push((y, x));
-            }
+    itertools::iproduct!(0..map.len(), 0..map[0].len()).for_each(|(y, x)| {
+        if get_neighbors((y, x), map.len(), map[0].len())
+            .iter()
+            .all(|p| map[p.0].chars().nth(p.1).unwrap() > map[y].chars().nth(x).unwrap())
+        {
+            low_points.push((y, x));
         }
-    }
-
+    });
     low_points
 }
 
@@ -49,9 +35,12 @@ fn get_basin_size(map: &[String], start: Point) -> i64 {
     while !queue.is_empty() {
         let pos = queue.pop_front().unwrap();
         if !visited.contains(&pos) {
-            for next in get_neighbor_points(map, pos).iter().filter(|(y, x)| {
-                map[*y].chars().nth(*x).unwrap() != '9' && !visited.contains(&(*y, *x))
-            }) {
+            for next in get_neighbors(pos, map.len(), map[0].len())
+                .iter()
+                .filter(|(y, x)| {
+                    map[*y].chars().nth(*x).unwrap() != '9' && !visited.contains(&(*y, *x))
+                })
+            {
                 queue.push_back(*next);
             }
             visited.insert(pos);
@@ -67,7 +56,7 @@ pub fn solve_2() -> Result<i64, Box<dyn Error>> {
         .map(ToString::to_string)
         .collect::<Vec<_>>();
 
-    let mut basins = get_low_points(&map)
+    let mut basins = get_lows(&map)
         .iter()
         .map(|p| get_basin_size(&map, *p))
         .collect::<Vec<_>>();
@@ -82,7 +71,7 @@ pub fn solve_1() -> Result<i64, Box<dyn Error>> {
         .map(ToString::to_string)
         .collect::<Vec<_>>();
 
-    Ok(get_low_points(&map)
+    Ok(get_lows(&map)
         .iter()
         .map(|p| (map[p.0].chars().nth(p.1).unwrap().to_digit(10).unwrap() + 1) as i64)
         .sum())
