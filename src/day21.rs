329338ebbc0639a8e50b6cb18e9c40d453185975
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2021-12-21 13:44:17 +0200

    Solve day 21.2

--- src/day21.rs
+++ src/day21.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Dice {
     value: u32,
@@ -24,7 +26,7 @@ impl Dice {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Player {
     pos: u32,
     score: u32,
@@ -48,8 +50,8 @@ impl Player {
     }
 }
 
-pub fn solve_1() -> u32 {
-    let players = include_str!("../input/21.txt")
+fn read_input() -> (Player, Player) {
+    let positions = include_str!("../input/21.txt")
         .lines()
         .map(|line| {
             line.split_once(':')
@@ -60,10 +62,13 @@ pub fn solve_1() -> u32 {
                 .unwrap()
         })
         .collect::<Vec<_>>();
-    assert!(players.len() == 2);
+    assert!(positions.len() == 2);
 
-    let mut p1 = Player::new(players[0]);
-    let mut p2 = Player::new(players[1]);
+    (Player::new(positions[0]), Player::new(positions[1]))
+}
+
+pub fn solve_1() -> u32 {
+    let (mut p1, mut p2) = read_input();
     let mut dice = Dice::new();
 
     for round in 1..u32::MAX {
@@ -88,6 +93,58 @@ pub fn solve_1() -> u32 {
     0
 }
 
-pub fn solve_2() -> i64 {
-    -1
+fn dice_rolls() -> HashMap<u32, u32> {
+    itertools::iproduct!(1..=3, 1..=3, 1..=3).fold(HashMap::new(), |mut acc, (r1, r2, r3)| {
+        *acc.entry(r1 + r2 + r3).or_default() += 1;
+        acc
+    })
+}
+
+fn count_wins(
+    rolls: &HashMap<u32, u32>,
+    round: u64,
+    exp: u64,
+    p1: &Player,
+    p2: &Player,
+    win1: &mut u64,
+    win2: &mut u64,
+) {
+    if p1.score() >= 21 {
+        *win1 += exp;
+        return;
+    }
+    if p2.score() >= 21 {
+        *win2 += exp;
+        return;
+    }
+    for (&roll, &num) in rolls {
+        let mut p1_clone = p1.clone();
+        let mut p2_clone = p2.clone();
+        if round % 2 == 1 {
+            p1_clone.advance(roll);
+        } else {
+            p2_clone.advance(roll);
+        }
+        count_wins(
+            rolls,
+            round + 1,
+            exp * u64::from(num),
+            &p1_clone,
+            &p2_clone,
+            win1,
+            win2,
+        );
+    }
+}
+
+pub fn solve_2() -> u64 {
+    let (p1, p2) = read_input();
+
+    let rolls = dice_rolls();
+    let mut win1 = 0;
+    let mut win2 = 0;
+
+    count_wins(&rolls, 1, 1, &p1, &p2, &mut win1, &mut win2);
+
+    std::cmp::max(win1, win2)
 }
